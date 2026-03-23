adbf63e921ebdc337877c2399480e603ab7cd3b8
steven <febriansteven1@gmail.com>
2026-03-23 16:04:18 +0700

    Fix return data jadi sorted Desc

---
+++ query/batch.sql
@@ -0,0 +1,21 @@
+INSERT INTO batch (nama_batch, status, tanggal_mulai, tanggal_selesai, "createdAt", "updatedAt") VALUES
+('Batch Pelatihan 2021A', false, '2021-01-01', '2021-03-01', NOW(), NOW()),
+('Batch Pelatihan 2021B', false, '2021-04-01', '2021-06-01', NOW(), NOW()),
+
+('Batch Pelatihan 2022A', false, '2022-01-01', '2022-03-01', NOW(), NOW()),
+('Batch Pelatihan 2022B', false, '2022-04-01', '2022-06-01', NOW(), NOW()),
+
+('Batch Pelatihan 2023A', false, '2023-01-01', '2023-03-01', NOW(), NOW()),
+('Batch Pelatihan 2023B', false, '2023-04-01', '2023-06-01', NOW(), NOW()),
+
+('Batch Pelatihan 2024A', false, '2024-01-01', '2024-03-01', NOW(), NOW()),
+('Batch Pelatihan 2024B', false, '2024-04-01', '2024-06-01', NOW(), NOW()),
+
+('Batch Pelatihan 2025A', false, '2025-01-01', '2025-03-01', NOW(), NOW()),
+('Batch Pelatihan 2025B', false, '2025-04-01', '2025-06-01', NOW(), NOW()),
+
+('Batch Pelatihan 2026A', false, '2026-01-01', '2026-03-01', NOW(), NOW()),
+('Batch Pelatihan 2026B', false, '2026-04-01', '2026-06-01', NOW(), NOW()),
+
+('Batch Pelatihan 2027A', false, '2027-01-01', '2027-03-01', NOW(), NOW()),
+('Batch Pelatihan 2027B', false, '2027-04-01', '2027-06-01', NOW(), NOW());
